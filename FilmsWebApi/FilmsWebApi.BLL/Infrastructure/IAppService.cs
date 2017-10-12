46e4e2925993fed95bfca5a2d2775c080d1c9522
Andrei Kukharenko <andrei-kukharenko@mail.ru>
2017-10-12 18:45:44 +0300

    *fixed AppService & IAppService

--- FilmsWebApi/FilmsWebApi.BLL/Infrastructure/IAppService.cs
+++ FilmsWebApi/FilmsWebApi.BLL/Infrastructure/IAppService.cs
@@ -1,8 +1,17 @@
 ﻿
+using FilmsWebApi.BLL.DTO;
+using System.Collections.Generic;
+
 namespace FilmsWebApi.BLL.Infrastructure
 {
-    public interface IFilmService
+    public interface IAppService
     {
+        IEnumerable<FilmDTO> GetAll();
 
+        FilmDTO GetFilmById(int id);
+
+        bool AddFilm(FilmDTO filmDTO);
+
+        IEnumerable<FilmDTO> SearchFilmsByTitle(string searchString);
     }
 }
